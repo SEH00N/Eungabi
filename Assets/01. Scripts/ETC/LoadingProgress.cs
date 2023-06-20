13160432683d786536725a4e93e7912524769fc4
SEH00N <06psh0501@gmail.com>
2023-06-20 11:13:49 +0900

    asdadasdasdasdasdasdasdasedsd

--- Assets/01. Scripts/ETC/LoadingProgress.cs
+++ Assets/01. Scripts/ETC/LoadingProgress.cs
@@ -5,9 +5,16 @@ public class LoadingProgress : MonoBehaviour
     [SerializeField] Transform startPos;
     [SerializeField] Transform endPos;
     [SerializeField] Transform playerTrm;
+    
+    [SerializeField, Range(0f, 1f)] float progress = 0f;
+
+    private void Update()
+    {
+        playerTrm.position = Vector3.Lerp(startPos.position, endPos.position, progress);
+    }
 
     public void SetProgress(float theta)
     {
-        playerTrm.position = Vector3.Lerp(startPos.position, endPos.position, theta);
+        progress = theta;
     }
 }
